082623a28638f1b6056cc869f54ab1266d61048c
GS <gsingh9@sapient.com>
2018-11-08 21:13:41 +0530

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # react-universal-app
+
 This example has react universal app design. Full capability to run server and client side code.
+
+## Prerequisites
+
+<ul>
+  <li><a href="https://nodejs.org">NodeJs</a></li>
+  <li><a href="http://babeljs.io">Babel</a></li>
+  <li><a href="https://webpack.js.org">Webpack</a></li>
+</ul>
+
+## Up and Running
+
+```
+// Install dependencies
+$ npm install
+
+// Start your project (in development mode)
+$ npm start
+
+// Create production build
+$ npm run build
+
+// Start your server (on created bundle)
+$ npm start
+```
---
+++ src/pages/HomePageComponent.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import { Helmet } from 'react-helmet';
+
+Array.prototype.random = function() {
+  return this[Math.floor(Math.random() * this.length)];
+};
+const colors = [
+  'red',
+  'green',
+  'aqua',
+  'Lime',
+  'grey',
+  'blue',
+  'orangered',
+  'coral',
+  'DeepPink',
+  'Crimson',
+  'Chocolate',
+  'Maroon',
+];
+
+class Home extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      selectedColor: 'black',
+    };
+  }
+  head() {
+    return (
+      <Helmet>
+        <title>React Boilerpalte | Homepage</title>
+      </Helmet>
+    );
+  }
+  changeColor() {
+    this.setState({
+      selectedColor: colors.random(),
+    });
+  }
+  clientSideMethod() {
+    this.setState({
+      selectedColor: colors.random(),
+    });
+  }
+  componentDidMount() {
+    this.clientSideMethod();
+  }
+  render() {
+    return (
+      <div>
+        {this.head()}
+        <h1>Homepage</h1>
+        <p>Some content</p>
+        <button
+          style={{ backgroundColor: this.state.selectedColor }}
+          onClick={() => this.changeColor()}
+        >
+          Change my color
+        </button>
+      </div>
+    );
+  }
+}
+
+export default Home;
